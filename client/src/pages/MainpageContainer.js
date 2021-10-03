2d64510aa1509fcc3d6bc5d848d0308544eac5e9
onniseonmi <soul012632@gmail.com>
2021-10-03 13:00:27 +0800

    Fixed nav and signin pages

--- client/src/pages/MainpageContainer.js
+++ client/src/pages/MainpageContainer.js
@@ -2,9 +2,9 @@ import React from 'react';
 import MainPopular from '../components/MainPopular';
 import MainToday from '../components/MainToday';
 
-export default function MypageContainer() {
+export default function MypageContainer({ signinCloseHandler }) {
   return (
-    <div>
+    <div className='main-body' onClick={signinCloseHandler}>
       <MainToday />
       <MainPopular />
     </div>
