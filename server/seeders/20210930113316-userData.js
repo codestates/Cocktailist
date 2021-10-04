18b99c9c7ca034ebdca1a5082cbea26ec01eb2fa
Daehee Yoon <allendy11@gmail.com>
2021-10-04 23:07:55 +0900

    fix models

--- server/seeders/20210930113316-userData.js
+++ server/seeders/20210930113316-userData.js
@@ -1,4 +1,4 @@
-'use strict';
+"use strict";
 
 module.exports = {
   up: async (queryInterface, Sequelize) => {
@@ -10,17 +10,19 @@ module.exports = {
      *   name: 'John Doe',
      *   isBetaMember: false
      * }], {});
-    */
-  await queryInterface.bulkInsert('users', [{
-    id: 1,
-    username: 'kimcording',
-    email: 'kimcording@codestates.com',
-    password: '1234',
-    mobile: '010-1234-5678',
-    image: '',
-    createdAt: new Date(),
-    updatedAt: new Date()
-  }])
+     */
+    await queryInterface.bulkInsert("users", [
+      {
+        id: 1,
+        username: "kimcording",
+        email: "kimcording@codestates.com",
+        password: "1234",
+        mobile: "010-1234-5678",
+        image: "",
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+    ]);
   },
 
   down: async (queryInterface, Sequelize) => {
@@ -30,6 +32,6 @@ module.exports = {
      * Example:
      * await queryInterface.bulkDelete('People', null, {});
      */
-    await queryInterface.bulkDelete('users', null, {})
-  }
+    await queryInterface.bulkDelete("users", null, {});
+  },
 };
