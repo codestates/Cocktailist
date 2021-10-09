bc2f31096d4596cf4a0b39499d28d4c8ee65c06e
onniseonmi <soul012632@gmail.com>
2021-10-09 14:21:35 +0800

    Fixed signin signup validations

--- client/src/pages/MypageContainer.js
+++ client/src/pages/MypageContainer.js
@@ -1,23 +1,8 @@
-import React, { useState } from 'react';
-import axios from 'axios';
+import React from 'react';
 
-export default function MypageContainer({ accessToken, setAccessToken }) {
-  const [email, setEmail] = useState('');
-  const [userName, setUserName] = useState('');
-  const [mobile, setMobile] = useState('');
-  const [image, setImage] = useState('');
-
-  const accessTokenRequest = () => {
-    const tokenUrl = 'http://localhost:8000/updateProfile';
-    axios
-      .get(tokenUrl, {
-        headers: { authorization: `Bearer ${accessToken}` },
-        withCredentials: true,
-      })
-      .then((res) => {
-        console.log(res);
-      });
-  };
+export default function MypageContainer({ userInfo }) {
+  console.log('mypage now');
+  console.log(userInfo);
   return (
     <div className='mypageContainer'>
       <div className='title'>Mypage</div>
@@ -25,29 +10,30 @@ export default function MypageContainer({ accessToken, setAccessToken }) {
       <br />
       <br />
       <div>
-        안녕하세요. <span className='name'>{email ? userName : 'Guest'}</span>
+        안녕하세요.{' '}
+        <span className='name'>
+          {userInfo.email ? userInfo.username : 'Guest'}
+        </span>
         님! jwt 로그인이 완료되었습니다.
       </div>
       <br />
       <br />
       <div className='item'>
-        <span className='item'>나의 이메일: </span> {email}
+        <span className='item'>나의 이메일: </span> {userInfo.email}
       </div>
       <div className='item'>
-        <span className='item'>username: </span> {userName}
+        <span className='item'>username: </span> {userInfo.username}
       </div>
       <div className='item'>
-        <span className='item'>mobile: </span> {mobile}
+        <span className='item'>mobile: </span> {userInfo.mobile}
       </div>
       <div className='item'>
-        <span className='item'>image: </span> {image}
+        <span className='item'>image: </span> {userInfo.image}
       </div>
       <br />
       <br />
       <div className='btnContainer'>
-        <button className='tokenBtn red' onClick={accessTokenRequest}>
-          access token request
-        </button>
+        <button className='tokenBtn red'>access token request</button>
         <button className='tokenBtn navy'>refresh token request</button>
       </div>
     </div>
