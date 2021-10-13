bebe28b27c214106bcea5ddd51022da424263bca
shane <freedommusic55@gmail.com>
2021-10-13 21:41:43 +0700

    mypage 구현

--- client/src/components/Favorite.js
+++ client/src/components/Favorite.js
@@ -1,14 +1,17 @@
 import React, { Component, useState ,useEffect} from 'react';
+import {Link} from 'react-router-dom'
 import axios from 'axios'
+import './Favorite.css'
 
-
+const ec2_url =
+  "http://ec2-3-35-22-42.ap-northeast-2.compute.amazonaws.com:8000";
 
 
 
 const Favorite =   ({accessToken}) => {
     const [favorites, setFavorites] = useState([])
     useEffect(()=>{
-        axios.get('http://localhost:8000/mypages/myFavorite',{
+        axios.get(`${ec2_url}/mypages/myFavorite`,{
          headers: { authorization: `Bearer ${accessToken}` }
              }, {
              withCredentials: true
@@ -19,15 +22,19 @@ const Favorite =   ({accessToken}) => {
 
    console.log(favorites)
    
+   const handleDelete = (favorite) => {
+       
+       axios.get(`${ec2_url}/mypages/deleteFavorite/${favorite.id}`)
+   }
+   const handleCocktailPage = (cocktail) =>{
+       console.log(cocktail)
+   }
 
-    
     
     return (
         <React.Fragment>
-
-         <h4>Favorites</h4>
-         <div >{favorites.map(el =><div key={el.id} ><img src = {el.image}/></div>)}</div>
-    
+         <h4 style={{padding:'20px 10px 10px 0'}}>Favorites</h4>
+         <div id='favorites-container' >{favorites.map(el =><div className ="favorite-item" key={el.id} ><img onClick={()=>handleCocktailPage(el)} src = {el.image}/><button onClick={()=> handleDelete(el)} >X</button></div>)}</div>
         </React.Fragment>
     )
 }
--- client/src/components/Mypost.js
+++ client/src/components/Mypost.js
@@ -1,11 +1,15 @@
 import React, { Component,useEffect,useState } from 'react';
 import axios from 'axios'
+import 'bootstrap/dist/css/bootstrap.css'
+
+const ec2_url =
+  "http://ec2-3-35-22-42.ap-northeast-2.compute.amazonaws.com:8000";
 
 const Mypost = ({accessToken}) => {
 
     const [post, setMypost] = useState([])
     useEffect(()=>{
-        axios.get('http://localhost:8000/mypages/myPost',{
+        axios.get(`${ec2_url}/mypages/myPost`,{
          headers: { authorization: `Bearer ${accessToken}` }
              }, {
              withCredentials: true
@@ -17,9 +21,10 @@ const Mypost = ({accessToken}) => {
     return (
         <React.Fragment>
 
-    <h4>My Post</h4>
-    
-         <div>{post.map(el =><div key={el.postId} ><img src = {el.image}/></div>)}</div>
+    <h4 style={{padding:'20px 10px 10px 0'}}>My Post</h4>
+    <span style={{margin:'20px'}}>  You have no post yet!</span>
+    <button  style={{margin:'10px 0 0 10px', backgroundColor:'gray' , padding:'5px',borderRadius:'5%'}} >Create new</button>
+         {/* <div>{post.map(el =><div key={el.postId} ><img src = {el.image}/></div>)}</div> */}
         </React.Fragment>
     )
 }
