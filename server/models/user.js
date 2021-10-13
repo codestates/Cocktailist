e189be6c281fca1469b0a902557563d2b6c6b265
ehdgusdl9177 <k23129177@gmail.com>
2021-10-13 15:06:04 +0900

    delete community

--- server/models/user.js
+++ server/models/user.js
@@ -8,10 +8,6 @@ module.exports = (sequelize, DataTypes) => {
      * The `models/index` file will call this method automatically.
      */
     static associate(models) {
-      models.user.hasMany(models.post, {
-        foreignKey: "userId",
-        onDelete: "cascade",
-      });
       models.user.hasMany(models.favorite, {
         foreignKey: "userId",
         onDelete: "cascade",
