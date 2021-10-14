b62457b76005b0608ccd0f18e866015085684935
Daehee Yoon <allendy11@gmail.com>
2021-10-14 12:15:40 +0900

    practice merge

--- server/controller/mypage/favorite.js
+++ server/controller/mypage/favorite.js
@@ -28,17 +28,17 @@ module.exports = {
   },
 
   deleteFavorite: async (req, res) => {
-    const { id } = req.params
-    console.log(id)
-    await favorite.destroy({
-      where: {
-        id
-      }
-    })
-    .then(() => {
-      res.sendStatus(201)
-    })
-  }
+    const { id } = req.params;
+    await favorite
+      .destroy({
+        where: {
+          id,
+        },
+      })
+      .then(() => {
+        res.sendStatus(201);
+      });
+  },
 };
 
 /*
